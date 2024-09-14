56fb933bcc7137118e8eda8d7e64fc3e3049967c
DrFish9 <kamielghaisan@gmail.com>
2024-09-15 01:06:17 +0700

    first commit

---
+++ PolygonCalculator.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    string useAgain;
+    double sideCount, sideLength, polygonArea, polygonCircumference;
+
+    cout << "n - sided regular polygon area calculator\nNumber of sides of your polygon(whole number): ";
+    
+    while (true)
+    {
+        cin >> sideCount;
+        sideCount = floor(sideCount);
+        cout << "Side length of your polygon: ";
+        cin >> sideLength;
+
+        polygonArea = (pow(sideLength, 2.0)*(sideCount))/(4*tan(M_PI/sideCount));
+        polygonCircumference = sideCount*sideLength;
+        cout << "The area of your polygon is: " << polygonArea << "\n";
+        cout << "The circumference of your polygon is: " << polygonCircumference << "\n";
+
+        cout << "do you want to keep going?[Y/n]\n";
+        cin >> useAgain;
+        if (useAgain == "Y")
+        {
+            continue;
+        }
+        break;
+    }
+    
+
+    return 0;
+    
+}
